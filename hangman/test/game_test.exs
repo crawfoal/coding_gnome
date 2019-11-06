d832eda8d7f9a65d5a6f389f27bf1e460d24413d
Amanda Dolan <amandacrawfordalc@gmail.com>
2019-11-06 06:46:47 -0700

    Add function to reveal guessed letters; clean up

--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -17,26 +17,25 @@ defmodule GameTest do
     assert game.letters |> Enum.all?(&( Regex.match?(~r([a-z]), &1) ))
   end
 
-  test "state isn't changed for :won game" do
+  test "state isn't changed for :won or :lost game" do
     for state <- [:won, :lost] do
       game = Game.new_game() |> Map.put(:game_state, state)
-      assert { ^game, _ } = Game.make_move(game, "x")
+      assert ^game = Game.make_move(game, "x")
     end
   end
 
   test "first occurrence of letter is not already used" do
-    { %{game_state: state}, _tally } = Game.new_game() |> Game.make_move("x")
+    %{game_state: state} = Game.new_game() |> Game.make_move("x")
     assert state != :already_used
   end
 
   test "second occurrence of letter is not already used" do
-    { game, _tally } = Game.new_game() |> Game.make_move("x")
-    assert { %{game_state: :already_used}, _tally } = Game.make_move(game, "x")
+    game = Game.new_game() |> Game.make_move("x")
+    assert %{game_state: :already_used} = Game.make_move(game, "x")
   end
 
   test "a good guess is recognized" do
-    game = Game.new_game("wibble")
-    {game, _tally} = Game.make_move(game, "w")
+    game = Game.new_game("wibble") |> Game.make_move("w")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
   end
@@ -46,17 +45,16 @@ defmodule GameTest do
     game =
       ~w(w i b l)
       |> Enum.reduce(game, fn letter, game ->
-        {game, _tally} = Game.make_move(game, letter)
+        game = Game.make_move(game, letter)
         assert game.game_state == :good_guess
         game
       end)
-    {game, _tally} = Game.make_move(game, "e")
+      |> Game.make_move("e")
     assert game.game_state == :won
   end
 
   test "a bad guess is recognized" do
-    game = Game.new_game("wibble")
-    {game, _tally} = Game.make_move(game, "x")
+    game = Game.new_game("wibble") |> Game.make_move("x")
     assert game.game_state == :bad_guess
     assert game.turns_left == 6
   end
@@ -66,11 +64,11 @@ defmodule GameTest do
     game =
       ~w(a b c d e f)
       |> Enum.reduce(game, fn letter, game ->
-        {game, _tally} = Game.make_move(game, letter)
+        game = Game.make_move(game, letter)
         assert game.game_state == :bad_guess
         game
       end)
-    {game, _tally} = Game.make_move(game, "g")
+      |> Game.make_move("g")
     assert game.game_state == :lost
   end
 end
