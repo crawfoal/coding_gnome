3c70843ef191558f118815b5f03176b84815d03b
Amanda Dolan <amandacrawfordalc@gmail.com>
2019-11-28 08:15:52 -0600

    Make dictionary a supervised application

--- dictionary/lib/dictionary/word_list.ex
+++ dictionary/lib/dictionary/word_list.ex
@@ -1,7 +1,13 @@
 defmodule Dictionary.WordList do
-  def random_word(word_list) do
-    word_list
-    |> Enum.random()
+
+  @name __MODULE__
+
+  def start_link() do
+    Agent.start_link(&word_list/0, name: @name)
+  end
+
+  def random_word() do
+    Agent.get(@name, &Enum.random/1)
   end
 
   def word_list() do
@@ -10,5 +16,6 @@ defmodule Dictionary.WordList do
     |> File.read!()
     |> String.split(~r/\n/)
   end
+
 end
 
